8ac0544a5d20dabcbb6c63e9594453f19c7099b0
Ashley Towns <mail@ashleytowns.id.au>
2015-12-05 03:49:09 +1100

    hmm much better

--- Sources/UdevEnumerator.swift
+++ Sources/UdevEnumerator.swift
@@ -12,33 +12,21 @@ public class UdevEnumerator : SequenceType {
   }
 
   public func generate() -> AnyGenerator<UdevDevice> {
-    var currentDevice: Optional<COpaquePointer> = wrapCFunc(udev_enumerate_get_list_entry)(self.handle)
+    var currentDevice: COpaquePointer? = udev_enumerate_get_list_entry(self.handle)
 
     return AnyGenerator {
       let newDev = currentDevice
                       .flatMap(self.listEntryGetName)
                       .map(self.udev.deviceFrom)
 
-      currentDevice = currentDevice.flatMap(self.wrapCFunc(udev_list_entry_get_next))
+      currentDevice = currentDevice.flatMap(udev_list_entry_get_next)
 
       return newDev
     }
   }
 
-  private func listEntryGetName(ptr: COpaquePointer) -> Optional<String> {
+  private func listEntryGetName(ptr: COpaquePointer) -> String? {
     let str = udev_list_entry_get_name(ptr)
     return String.fromCString(str)
-  }
-
-  private func wrapCFunc(f: (COpaquePointer) -> COpaquePointer) -> ((COpaquePointer) -> Optional<COpaquePointer>) {
-    func inner(p: COpaquePointer) -> Optional<COpaquePointer> {
-      let val = f(p)
-      if val != nil {
-        return Optional.Some(val)
-      } else {
-        return Optional.None
-      }
-    }
-    return inner
   }
 }
